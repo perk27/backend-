282b235b52de46578cf5ba6cfc5be3b8314f5770
perk271 <hohalokaka@gmail.com>
2025-09-15 18:41:13 +0700

    why

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5001;
 
 //middleware
 app.use(cors({
-    origin: "https://kakanote.pages.dev/",})
+    origin: "*",})
 );
 app.use(express.json());
 app.use(rateLimiter);
